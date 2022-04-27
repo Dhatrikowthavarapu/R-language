c09a482ab7f95afa0456bb080cdae214c0a4ed9b
KOWTHAVARAPU DHATRI PHANI PRIYA <94949658+Dhatrikowthavarapu@users.noreply.github.com>
2022-04-27 16:31:20 +0530

    Create Basics.r

---
+++ Basics.r
@@ -0,0 +1,73 @@
+3+4
+1+1
+1+2+3
+3*7*2
+4/2
+4/3
+4*6+5
+(4*6)+5
+4*(6+5)
+x<-2
+x
+y=5
+y
+3->z
+z
+a<-b<-7
+a
+b
+assign("j",4)
+j
+rm(j)
+j
+theVariable<- 17
+theVariable
+THEVARIABLE
+classs(x)
+rm(i)
+i<- 5L
+i
+is.integer(i)
+is.numeric(i)
+class(4L)
+class(2.8)
+4L * 2.8
+class(4L * 2.8)
+class(5L)
+class(2L)
+5L/2L
+class(5L/2L)
+x<- "data"
+x
+nchar(x)
+nchar("hello")
+nchar(3)
+nchar(453)
+nchar(y)
+date1<-as.Date("2012-06-28")
+date1
+class(date1)
+as.numeric(date1)
+date2<-as.POSIXct("2012-06-28 17:42")
+class(date2)
+as.numeric(date2)
+class(date2)
+class(as.numeric(date1))
+TRUE*5
+FALSE*5
+k<-TRUE
+k
+class(k)
+is.logical(k)
+TRUE
+TRUE
+T
+class(T)
+T <- 7
+T
+2==3
+2!=3
+2<3
+2<=3
+“data”==“stats”
+”data”<“stats”
